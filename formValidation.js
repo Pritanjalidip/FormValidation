284728db092d9e8abc11662b52e669da46b267f4
Pritanjalidip <dippritanjali@outlook.com>
2024-08-03 14:26:06 +0530

    Add files via upload

---
+++ formValidation.js
@@ -0,0 +1,96 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if (validateInputs()) {
+        form.submit();
+    }
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    let isValid = true;
+
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const phoneValue = phone.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+        isValid = false;
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+        isValid = false;
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+        isValid = false;
+    } else {
+        setSuccess(email);
+    }
+
+    if(phoneValue === '') {
+        setError(phone, 'Phone number is required');
+        isValid = false;
+    } else if (phoneValue.length !== 10 || !/^\d{10}$/.test(phoneValue)) {
+        setError(phone, 'Phone number must be 10 digits long');
+        isValid = false;
+    } else {
+        setSuccess(phone);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+        isValid = false;
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 characters.');
+        isValid = false;
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+        isValid = false;
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords don't match");
+        isValid = false;
+    } else {
+        setSuccess(password2);
+    }
+
+    return isValid;
+};
